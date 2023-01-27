45104cfda794b2d24be8b36fd0376f07b8625326
mazufik <taufikprincecode10@gmail.com>
2023-01-27 16:17:05 +0900

    Login JWT Frontend

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import AxiosTarget from '../hook/AxiosTarget';
+import Navbar from './Navbar';
+
+const Dashboard = () => {
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
+  const [users, setUsers] = useState([]);
+  const history = useNavigate();
+
+  useEffect(() => {
+    refreshToken();
+    getUsers();
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get(AxiosTarget[2]);
+      const accessToken = response.data.accessToken;
+      setToken(accessToken);
+      const decoded = jwt_decode(accessToken);
+      setName(decoded.name);
+      setExpire(decoded.exp);
+    } catch (error) {
+      if (error.response) {
+        history('/');
+      }
+    }
+  };
+
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+        const response = await axios.get(AxiosTarget[2]);
+        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setToken(response.data.accessToken);
+        const decoded = jwt_decode(response.data.accessToken);
+        setName(decoded.name);
+        setExpire(decoded.exp);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const getUsers = async () => {
+    const response = await axiosJWT.get(AxiosTarget[0], {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setUsers(response.data);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className='container mt-5'>
+        <h1> Welcome Back : {name}</h1>
+        <button onClick={getUsers} className='button is-info'>
+          Get Users
+        </button>
+        <table className='table is-striped is-fullwidth'>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/hook/AxiosTarget.js
@@ -0,0 +1,8 @@
+const AxiosTarget = [
+  'http://localhost:5000/users',
+  'http://localhost:5000/login',
+  'http://localhost:5000/token',
+  'http://localhost:5000/logout',
+];
+
+export default AxiosTarget;
